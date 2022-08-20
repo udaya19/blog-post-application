6985625f95c2871a85e5b85e96f67c33e690799e
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-08-20 16:27:50 +0530

    Adding NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,16 @@
+import { Link } from 'react-router-dom';
 import React from 'react';
 function NavBar() {
     return (  
         <div>
-            NavBar
+            <ul id='nav'>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/create-post">Create Post</Link>
+                </li>
+            </ul>
         </div>
     );
 }
